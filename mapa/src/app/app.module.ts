818417b2b588f18534010b58bd56f36e873c957e
knoppf1 <kerstnerfabiano@gmail.com>
2022-05-24 10:34:42 -0300

    Dados CONSEG

--- mapa/src/app/app.module.ts
+++ mapa/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AgmCoreModule } from '@agm/core';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDrCpM0yGH_iMvBFna1gU1voQR-I7PGUgQ'
     })
---
+++ mapa/src/app/app.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import{ tap} from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  private readonly apiUrl: string = 'http://localhost:3000/';
+
+  constructor(
+    private http: HttpClient,
+    private router:Router) { }
+
+
+  adicionar(data: any): Observable<any> {return this.http.post(this.apiUrl + 'cadastros', data);
+  }
+
+  listar():Observable<any> {return this.http.get(this.apiUrl + 'cadastros').pipe(
+    tap(cadastros =>console.log('Res lista Favoritotos', cadastros))
+  );
+  }
+
+  delete(id: number): Observable<any> {
+    console.log('deletando', id)
+    return this.http.delete(this.apiUrl + 'cadastros/'+id);
+  }
+
+  editar(id: number, data: any): Observable<any> {
+    console.log('id',id);
+    console.log('dados',data);
+    return this.http.put(this.apiUrl+'cadastros/'+id,data);
+  }
+
+  buscar(id: number): Observable<any> {
+    return this.http.get(this.apiUrl+'cadastros/'+id);
+  }
+}
